3addb043bd0bd3f41e9f52882808c70acf430ee9
KevinSplits <130183913+KevinSplits@users.noreply.github.com>
2025-06-30 16:58:55 -0500

    eso mismo

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,6 +9,13 @@ import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.co
 import { EmpleadosComponent } from './empleados/empleados.component';
 import { EditarClienteComponent } from './editar-cliente/editar-cliente.component';
 import { EditarEmpleadoComponent } from './editar-empleado/editar-empleado.component';
+import { EditarProductoComponent } from './editar-producto/editar-producto.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { NuevaVentaComponent } from './nueva-venta/nueva-venta.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { VentasComponent } from './ventas/ventas.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +25,14 @@ import { EditarEmpleadoComponent } from './editar-empleado/editar-empleado.compo
     AgregarEmpleadoComponent,
     EmpleadosComponent,
     EditarClienteComponent,
-    EditarEmpleadoComponent
+    EditarEmpleadoComponent,
+    EditarProductoComponent,
+    HeaderComponent,
+    HomeComponent,
+    InventarioComponent,
+    NuevaVentaComponent,
+    SidebarComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule,
