cf2d208cfea921046d6b85c787a2a95f1bd497ae
KevinSplits <130183913+KevinSplits@users.noreply.github.com>
2025-06-30 16:38:01 -0500

    pasando algunas cosas

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component'; // importa tu componente
+import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './editar-empleado/editar-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'clientes', component: ClientesComponent }, 
+  { path: 'clientes/agregar', component: AgregarClienteComponent } ,
+  { path: 'clientes/editar', component: EditarEmpleadoComponent }
+  , { path: 'empleados', component: EmpleadosComponent }
+  , { path: 'empleados/agregar', component: AgregarEmpleadoComponent }
+  , { path: 'empleados/editar', component: EditarEmpleadoComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { AgregarClienteComponent } from './agregar-cliente/agregar-cliente.component';
+import { AgregarEmpleadoComponent } from './agregar-empleado/agregar-empleado.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EditarClienteComponent } from './editar-cliente/editar-cliente.component';
+import { EditarEmpleadoComponent } from './editar-empleado/editar-empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    AgregarClienteComponent,
+    AgregarEmpleadoComponent,
+    EmpleadosComponent,
+    EditarClienteComponent,
+    EditarEmpleadoComponent
   ],
   imports: [
     BrowserModule,
